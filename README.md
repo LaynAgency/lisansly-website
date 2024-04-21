3344be9ee348ad9c3bbf4ca1c76ce3c286f1b243
yali <tangozyusufali@gmail.com>
2024-04-21 14:14:18 +0300

    feat: init app

---
+++ README.md
@@ -0,0 +1,1 @@
+# lisansly-landing-page
---
+++ app.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts">
+const colorMode = useColorMode();
+colorMode.value = "dark";
+useSeoMeta({
+  title: "Lisansly",
+});
+useHead({
+  bodyAttrs: {
+    class: "bg-black",
+  },
+});
+</script>
+
+<template></template>
