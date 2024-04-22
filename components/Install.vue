8885d406df5b6f425771cba38fb6f60b61d12942
yali <tangozyusufali@gmail.com>
2024-04-22 13:08:09 +0300

    style: update install

--- components/Install.vue
+++ components/Install.vue
@@ -17,17 +17,19 @@ const items = [
 
 <template>
   <div class="grid place-items-center mt-36">
-    <UTabs :items="items" class="w-64">
+    <UTabs :items="items" class="w-72">
       <template #default="{ item }">
         <UIcon :name="item.icon" size="20" />
       </template>
       <template #item="{ item }">
         <div
-          class="mt-7 flex items-center justify-between py-3 pl-7 pr-4 font-mono rounded-full border-4 bg-black border-zinc-800 drop-shadow-[0_0_20px_rgba(255,255,255,.2)]"
+          class="text-nowrap mt-7 py-3 px-5 flex items-center font-mono rounded-full border-4 bg-black border-zinc-800 drop-shadow-[0_0_20px_rgba(255,255,255,.2)]"
         >
+          <UIcon name="ph:terminal" size="20" class="mr-3" />
           {{ item.label }}
           <UButton
             @click="copyToClipboard(item.label)"
+            class="ml-auto"
             variant="link"
             color="gray"
           >
